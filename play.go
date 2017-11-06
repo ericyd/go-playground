af6eba27df609eb535b490a6c4721919d6c636e7
Eric <eric@ericyd.com>
2017-11-05 20:59:18 -0800

    playing with first class functions

--- play.go
+++ play.go
@@ -6,5 +6,15 @@ func main() {
 	fmt.Println("this is a test")
 
 	// in the same pkg, does not need explicit import
-	extraFunc()
+	z := add(2, 3)
+	fmt.Printf("z = %d\n", z)
+
+	// first class functions?
+	add2 := curryAdd(2)
+	_ = curryAdd(3)
+
+	fmt.Println(add2(7))
+
+	one := firstClass(add2)
+	fmt.Println(one)
 }
--- play2.go
+++ play2.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func extraFunc() {
-	fmt.Println("printing from extra func")
+func extra() {
+	fmt.Println("extra print")
 }
+
+func add(x, y int) (z int) {
+	z = x + y
+	return
+}
+
+// Rules:
+// 1. it kind of uses a functional-eque function defition
+// e.g. a -> b -> c
+// curryAdd returns a function with parameters (b int) which itself returns an int
+// 2. The returned function cannot be named
+func curryAdd(a int) func(b int) int {
+	return func(b int) int {
+		return add(a, b)
+	}
+}
+
+// huh, this is definitely different
+// if you want to pass a function to another function, you need to
+// specify it's signature in the parameter
+func firstClass(f func(b int) int) int {
+	f(2)
+	return 1
+}
+
+// func curry(f func(vars ...interface{}) func(vars ...interface{}) interface{}) {
+// 	return
+// }
